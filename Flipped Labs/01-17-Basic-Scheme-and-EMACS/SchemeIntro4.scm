e7fb50387866fb179869fd5789ad0222eb71ed9f
Tizzie <Tizzie@users.noreply.github.com>
2020-02-05 16:23:47 -0600

    Updated Flipped Lab Files
    - Updated 01/17 Flipped Lab #4
    - Added 01/24 Flipped Lab #2

--- Flipped Labs/01-17-Basic-Scheme-and-EMACS/SchemeIntro4.scm
+++ Flipped Labs/01-17-Basic-Scheme-and-EMACS/SchemeIntro4.scm
@@ -1,14 +1,8 @@
-; Xchel Diaz
-; 01/17/2020
-
 ;(define (fact n) (if (< n 2) 1 (* n (fact (- n 1 )))))
 ;(trace fact)
 ;(fact 5)
 
-(define (fib m) (if (< m 3) 1 (+ (fib (- m 2)) (fib ( - m 1 )))))
+(define (fib m) (if (< m 3) 1 (+ (fib (- m 1)) (fib (- m 2)))))
 (trace fib)
-(display (fib 10))
-;(newline)
-;(display (fib 10)
-
+(fib 10)
 (exit)
---
+++ Flipped Labs/01-24-Basic-Scheme-Functions/SchemeFlipped2.scm
@@ -0,0 +1,8 @@
+(define (simpleParameter x)
+(display x)
+)
+
+(simpleParameter 21)
+(newline)
+(simpleParameter "Hello")
+(newline)
